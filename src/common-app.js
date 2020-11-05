a93b3adecd597a66341f428ab94f10cc04253e5c
Cameron Rollheiser <cameron.rollheiser@artsymail.com>
2020-11-05 11:29:32 -0500

    Fix types, cleanup middleware

--- src/common-app.js
+++ src/common-app.js
@@ -5,22 +5,87 @@ import "./lib/DOMParser"
 // TODO: Publish artsy morgan as an npm module.
 import logger from "artsy-morgan"
 import express from "express"
-import commonMiddlewareSetup from './common-middleware'
+import RavenServer from "raven"
+import commonMiddlewareSetup from "./common-middleware"
+import errorHandlingMiddleware from "./lib/middleware/error_handler"
+import config from "./config"
+
+const { SENTRY_PRIVATE_DSN } = config
 
 const app = express()
 
 app.set("trust proxy", true)
 
+// Log all routes
+// TODO: Disable asset route logging.
 app.use(logger)
 
+// *****************************************************************************
 // Install common middlewares
+// *****************************************************************************
 commonMiddlewareSetup(app)
 
-// novo must be first, the current app contains a catch all route.
+// *****************************************************************************
+// Blank page used by Eigen for caching web views.
+//
+// See: https://github.com/artsy/microgravity-private/pull/1138
+// Does this need to come before middleware?
+// *****************************************************************************
+app.use(require("desktop/apps/blank"))
+
+// *****************************************************************************
+// Health Check
+//
+// TODO: Move to own function
+// *****************************************************************************
+
+// Routes for pinging system time and up
+app.get("/system/time", (req, res) =>
+  res.status(200).send({ time: Date.now() })
+)
+app.get("/system/up", (req, res) => {
+  res.status(200).send({ nodejs: true })
+})
+
+// *****************************************************************************
+// Force (Novo)
+// *****************************************************************************
 import novo from "./novo/index"
 app.use("/", novo)
 
+// *****************************************************************************
+// Force (Current)
+// *****************************************************************************
 import current from "./current"
 app.use("/", current)
+
+// *****************************************************************************
+// Error Handling
+//
+// TODO: Move to a its own function.
+// *****************************************************************************
+
+// 404 handler
+app.get("*", (req, res, next) => {
+  const err = new Error()
+  err.status = 404
+  err.message = "Not Found"
+  next(err)
+})
+
+// Error handling
+
+// FIXME: Investigate issue with new Sentry middleware. See note near import.
+// Sentry error handling appear above other middleware
+// if (SENTRY_PUBLIC_DSN) {
+//   app.use(Sentry.Handlers.errorHandler())
+// }
+
+// Old Sentry SDK.
+if (SENTRY_PRIVATE_DSN) {
+  app.use(RavenServer.errorHandler())
+}
+
+app.use(errorHandlingMiddleware)
 
 module.exports = app
